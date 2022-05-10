f14309d2f790e661c63a36229e2e24076490621d
Michel <micheldavrdz@gmail.com>
2022-05-09 20:43:26 -0500

    update: linter fixes

--- app/lib/services/StudentService.js
+++ app/lib/services/StudentService.js
@@ -1,15 +1,15 @@
 class StudentService {
-    static getStudents(students) {
-        return students;
-    }
+  static getStudents(students) {
+    return students;
+  }
 
-    static getCertifiedStudents(students) {
-        return students.filter(student => student.haveCertification === true);
-    }
+  static getCertifiedStudents(students) {
+    return students.filter((student) => student.haveCertification === true);
+  }
 
-    static getStudentsCreditsAbove500(students) {
-        return students.filter(student => student.credits > 500);
-    }
+  static getStudentsCreditsAbove500(students) {
+    return students.filter((student) => student.credits > 500);
+  }
 }
 
 module.exports = StudentService;
