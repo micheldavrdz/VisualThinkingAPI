0bc37ac3f5e77e751ac473ab357748efd610f51d
Michel <micheldavrdz@gmail.com>
2022-05-10 13:22:17 -0500

    update: changed 'defined' for 'in files'

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,7 +1,7 @@
 const Reader = require('../../app/lib/utils/Reader');
 
 describe('Unit test for Reader class', () => {
-  test('Test case 1 - Check if visualpartners is defined', () => {
+  test('Test case 1 - Check if visualpartners is in files', () => {
     const visualpartners = Reader.readJsonfile('visualpartners.json');
     expect(visualpartners).toBeDefined();
     expect(visualpartners.length).toBeGreaterThan(0);
